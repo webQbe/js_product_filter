b3b1126939afc420975c1e1dc0c087ac56c5ebdc
webQbe <webcubesolutionslk@gmail.com>
2024-12-01 21:00:56 +0530

    Defined products array

---
+++ script.js
@@ -0,0 +1,75 @@
+// Define Products Array
+const products = [
+    {
+      name: 'Sony Playstation 5',
+      url: 'images/playstation_5.png',
+      category: 'games',
+      price: 499.99,
+    },
+    {
+      name: 'Samsung Galaxy',
+      url: 'images/samsung_galaxy.png',
+      category: 'smartphones',
+      price: 399.99,
+    },
+    {
+      name: 'Cannon EOS Camera',
+      url: 'images/cannon_eos_camera.png',
+      category: 'cameras',
+      price: 749.99,
+    },
+    {
+      name: 'Sony A7 Camera',
+      url: 'images/sony_a7_camera.png',
+      category: 'cameras',
+      price: 1999.99,
+    },
+    {
+      name: 'LG TV',
+      url: 'images/lg_tv.png',
+      category: 'televisions',
+      price: 799.99,
+    },
+    {
+      name: 'Nintendo Switch',
+      url: 'images/nintendo_switch.png',
+      category: 'games',
+      price: 299.99,
+    },
+    {
+      name: 'Xbox Series X',
+      url: 'images/xbox_series_x.png',
+      category: 'games',
+      price: 499.99,
+    },
+    {
+      name: 'Samsung TV',
+      url: 'images/samsung_tv.png',
+      category: 'televisions',
+      price: 1099.99,
+    },
+    {
+      name: 'Google Pixel',
+      url: 'images/google_pixel.png',
+      category: 'smartphones',
+      price: 499.99,
+    },
+    {
+      name: 'Sony ZV1F Camera',
+      url: 'images/sony_zv1f_camera.png',
+      category: 'cameras',
+      price: 799.99,
+    },
+    {
+      name: 'Toshiba TV',
+      url: 'images/toshiba_tv.png',
+      category: 'televisions',
+      price: 499.99,
+    },
+    {
+      name: 'iPhone 14',
+      url: 'images/iphone_14.png',
+      category: 'smartphones',
+      price: 999.99,
+    },
+  ];
